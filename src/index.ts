b7224f508c80f5703bdccbcc3a4181eb4c375596
Damir Nasibullin <damir.nas@prpl.global>
2021-06-15 22:40:55 +0500

    learn workers, add .ts implementation for workers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello world')
+import express from 'express';
+import { Worker } from 'worker_threads';
+
+const app = express();
+ 
+const worker = new Worker('./src/worker.js', {
+  workerData: {
+    value: 15,
+    path: './worker.ts'
+  }
+});
+
+worker.on('message', (result) => {
+  console.log('message - ', result);
+});
+
+app.get('/', (req,res) => res.send('Hello from nodejs app'));
+
+app.listen(3000, () => console.log('Server on'));
+
